354930838e5ae30bb6985113b6881f0697c04a06
DeborL17065 <60325411+DeborL17065@users.noreply.github.com>
2020-03-09 09:30:01 -0600

    Create ADC.h

---
+++ Luxometro.X/ADC.h
@@ -0,0 +1,17 @@
+/* 
+ * File:   ADC.h
+ * Author: debor
+ *
+ * Created on February 12, 2020, 12:02 AM
+ */
+
+#ifndef ADC_H
+#define	ADC_H
+
+uint8_t SENSOR1;
+
+void CONF_ADC(void);
+
+
+#endif	/* ADC_H */
+
