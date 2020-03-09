daacb4b948a21de83189d00395dd260344b7fb07
DeborL17065 <60325411+DeborL17065@users.noreply.github.com>
2020-03-09 09:31:32 -0600

    Update MLX90614.c

--- MASTER.X/MLX90614.c
+++ MASTER.X/MLX90614.c
@@ -23,20 +23,17 @@
 * 0x07 Para la temperatura del objeto
 * Retorna el valor de temperatura en grados Kelvin.
 *****************************************************************************/
-
 float Leer_Sensor(char Temp_Source){
     
     unsigned int Temp_var;
     
     I2C_Master_Start();
-   // I2C_Master_RepeatedStart();
     I2C_Master_Write(_IR_THERMO_ADDR << 1);
     I2C_Master_Write(Temp_Source);
     I2C_Master_RepeatedStart();
     I2C_Master_Write(_IR_THERMO_ADDR << 1);
     Temp_var = I2C_Master_Read(0);
     Temp_var = (I2C_Master_Read(0) << 8) + Temp_var;
-    //        I2C_Master_Write(0x0F);
     I2C_Master_Stop();                  
  
   return Temp_var;
@@ -47,21 +44,26 @@ float Leer_Sensor(char Temp_Source){
 *****************************************************************************/
 void Mostrar_Temperatura(char Temp_Source, float temperatura){
     
-    char text[15];
+    char text[5],SA[5],SB[5],SC[5],PUNTO1[5];
   //sprintf(text,"%2.1f", temperatura);
  
     if (Temp_Source == _AMB_TEMP){
         LCD_XY(1,2);
-        LCD_Cadena(text);//manda el dato a la LCD
-//        Lcd_Out(2, 2, text);
-//        Lcd_Chr(2,6,223);
-//        Lcd_Chr(2,7,0x43);
+        NUM1 = (temperatura)*10;
+        A = NUM1%10;   //8
+        itoa(SA,A,10);
+        B = (NUM1/10)%10;//9
+        itoa(SB,B,10);
+        C = (NUM1/100)%10;//1 
+        itoa(SC,C,10);
+        strcpy(PUNTO1,"."); //se coloca el caracter dentro del char
+        strcat(PUNTO1,SA); //se concatenan ambos chars
+        strcat(SB,PUNTO1); //se concatenan ambos chars
+        strcat(SC,SB); //se concatenan ambos chars
+        LCD_Cadena(SC);//manda el dato a la LCD
     }
     if (Temp_Source == _OBJ_TEMP){
         LCD_XY(1,2);
         LCD_Cadena(text);//manda el dato a la LCD
-//        Lcd_Out(2, 10, text);
-//        Lcd_Chr(2,14,223);
-//        Lcd_Chr(2,15,0x43);
     }
 }
