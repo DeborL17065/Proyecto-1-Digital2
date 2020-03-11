a7a1a4af564349934f3ab8cd4ecdab4dd9593772
DeborL17065 <60325411+DeborL17065@users.noreply.github.com>
2020-03-11 15:14:52 -0600

    Create SPI.h

---
+++ SPI.X/SPI.h
@@ -0,0 +1,17 @@
+/* 
+ * File:   SPI.h
+ * Author: debor
+ *
+ * Created on February 11, 2020, 11:28 PM
+ */
+
+#ifndef SPI_H
+#define	SPI_H
+char RANDOM;
+void CONF_SPI(void);
+void SPIWRITE (char dato);
+void SPIREAD (void);
+
+
+#endif	/* SPI_H */
+
