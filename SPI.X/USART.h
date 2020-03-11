aed4c2349e55b3fd1dfef14ee0a28fac0607005d
DeborL17065 <60325411+DeborL17065@users.noreply.github.com>
2020-03-11 15:14:58 -0600

    Create USART.h

---
+++ SPI.X/USART.h
@@ -0,0 +1,17 @@
+/* 
+ * File:   USART.h
+ * Author: debor
+ *
+ * Created on February 11, 2020, 11:30 PM
+ */
+
+#ifndef USART_H
+#define	USART_H
+
+
+void _RX_TX (void);
+void SerialSendChar (char CARACTER);
+void SerialSendString(char *txbuffer);
+
+#endif	/* USART_H */
+
