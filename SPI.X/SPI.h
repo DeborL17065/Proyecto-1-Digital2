6c99d7bd82ad0c40385e2182f561e4546dc73ca5
DeborL17065 <60325411+DeborL17065@users.noreply.github.com>
2020-05-11 23:10:28 -0600

    Update SPI.h

--- SPI.X/SPI.h
+++ SPI.X/SPI.h
@@ -2,15 +2,24 @@
  * File:   SPI.h
  * Author: debor
  *
- * Created on February 11, 2020, 11:28 PM
+ * Created on February 12, 2020, 12:01 AM
  */
 
 #ifndef SPI_H
 #define	SPI_H
+
+#define SDO  RC5
+#define SDI  RC4
+#define SCK  RC3
+#define SS   RA5
+
 char RANDOM;
-void CONF_SPI(void);
+
 void SPIWRITE (char dato);
+void CONF_SPI(void);
 void SPIREAD (void);
+
+
 
 
 #endif	/* SPI_H */
