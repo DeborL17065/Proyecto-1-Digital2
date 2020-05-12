fcc4fb0cc0660269dd47c9a971d8e2e786ca4efb
DeborL17065 <60325411+DeborL17065@users.noreply.github.com>
2020-05-11 23:10:13 -0600

    Update main.c

--- SPI.X/main.c
+++ SPI.X/main.c
@@ -39,65 +39,176 @@
 void init(void);
 
 int DEC_INC ;
+int S =0;
+
+int S_HUMEDAD,S_NIVEL,S_LLUVIA,S_LUZ,S_TEMPERATURA,S_TEMPERATURA1, S_ULTRASONICO;
+
+void __interrupt() isr(void){
+     
+    if (PIR1bits.SSPIF == 1){
+        SPIREAD();
+        if(RANDOM ==0x51){
+            SPIWRITE(S_TEMPERATURA);
+            SSPSTATbits.BF =0;
+            PIR1bits.SSPIF =0;
+          //  return;
+        }
+        SPIREAD();
+        if(RANDOM ==0x50){
+            SPIWRITE(S_LUZ); //Luz
+            //PORTDbits.RD0 =1;
+            //i=1;
+            SSPSTATbits.BF =0;
+            PIR1bits.SSPIF =0;
+          //  return;
+        }
+        SPIREAD();
+        if(RANDOM ==0x52){
+            SPIWRITE(S_HUMEDAD); //Humedad
+            //PORTDbits.RD0 =1;
+            //i=1;
+            SSPSTATbits.BF =0;
+            PIR1bits.SSPIF =0;
+         //   return;
+        }
+        SPIREAD();
+        if(RANDOM ==0x53){
+            SPIWRITE(S_LLUVIA);
+            //PORTDbits.RD0 =1;
+            //i=1;
+            SSPSTATbits.BF =0;
+            PIR1bits.SSPIF =0;
+         //   return;
+        }
+        SPIREAD();
+        if(RANDOM ==0x54){
+            SPIWRITE(S_NIVEL);
+            //PORTDbits.RD0 =1;
+            //i=1;
+            SSPSTATbits.BF =0;
+            PIR1bits.SSPIF =0;
+         //   return;
+        }
+        SPIREAD();
+        if(RANDOM ==0x55){
+            SPIWRITE(S_ULTRASONICO);
+            //PORTDbits.RD0 =1;
+            //i=1;
+            SSPSTATbits.BF =0;
+            PIR1bits.SSPIF =0;
+         //   return;
+        }
+        SSPSTATbits.BF =0;
+        PIR1bits.SSPIF =0;
+        return;
+    // return;
+    }
+    
+    if (PIR1bits.RCIF ==1 ){
+        
+        if(S==0){
+            S_TEMPERATURA = SERIALREAD();
+            PIR1bits.RCIF =0;
+            PORTDbits.RD0 =0;
+            S=1;
+           // return;
+        }
+        
+        if(S==1){
+            S_LUZ = SERIALREAD();
+            PIR1bits.RCIF =0;
+            PORTDbits.RD0 =1;
+            S=2;
+           // return;
+        }
+        if(S==2){
+            S_HUMEDAD = SERIALREAD();
+            PIR1bits.RCIF =0;
+            PORTDbits.RD0 =0;
+            S=3;
+          //  return;
+        }
+        if(S==3){
+            S_LLUVIA = SERIALREAD();
+            PIR1bits.RCIF =0;
+            PORTDbits.RD0 =0;
+            S=4;
+          //  return;
+        }
+        if(S==4){
+            S_NIVEL = SERIALREAD();
+            PIR1bits.RCIF =0;
+            PORTDbits.RD0 =0;
+            S=5;
+          //  return;
+        }
+        if(S==5){
+            S_ULTRASONICO = SERIALREAD();
+            PIR1bits.RCIF =0;
+            PORTDbits.RD0 =0;
+            S=0;
+          //  return;
+        }
+        PIR1bits.RCIF =0;
+        return;
+
+    }
+    
+
+ 
+
+}
 
 void main(void) {
     init();
     CONF_SPI();
     _RX_TX();
-
-    while (1){ 
-
-        PORTAbits.RA4 = 0;       //Slave Select
-        SPIWRITE(25); //ENVIAR EL DATO
-        SPIREAD(); //LEEMOS EL DATO DEL ESCLAVO  
+    while (1){   
         
-        PORTAbits.RA4 = 1;       //Slave Deselect 
+        SerialSendChar(S_TEMPERATURA);
         __delay_ms(1);
- 
+        SerialSendChar(S_LUZ);
+        __delay_ms(1);
+        SerialSendChar(S_HUMEDAD);
+        __delay_ms(1);
+        SerialSendChar(S_LLUVIA);
+        __delay_ms(1);
+        SerialSendChar(S_NIVEL);
+        __delay_ms(1);
+        SerialSendChar(S_ULTRASONICO);
+        __delay_ms(1);
+//       
+       
+
+//        PORTAbits.RA4 = 0;       //Slave Select
+//        SPIWRITE(25); //ENVIAR EL DATO
+//        SPIREAD(); //LEEMOS EL DATO DEL ESCLAVO  
+//        
+//        PORTAbits.RA4 = 1;       //Slave select 
+//        __delay_ms(1);
+// 
     
     }
     return;     
 }
 
-void __interrupt() isr(void){
 
-    if (PIR1bits.RCIF ==1){
-        if(RCSTAbits.OERR ==1){
-            RCSTAbits.OERR =0;
-            __delay_us(10);
-        }
-        else {
-            DEC_INC = RCREG; //ASIGNAMOS EL DATO SERIAL
-            PORTD = DEC_INC; //ASIGNAMOS EL DATO SERIAL AL PORTD
-        }
-    }
-    __delay_ms(1);
-    PORTAbits.RA4 = 0;       //Slave Select
-    SPIWRITE(25); //ENVIAR EL DATO
-    SPIREAD();
- 
-    SerialSendChar(RANDOM);   
-    __delay_ms(1);
-    PORTAbits.RA4 = 1;       //Slave Deselect 
- 
-    
-
-
-}
 
 void init(void) { 
     ///8MH
     OSCCONbits.IRCF0 = 1;
     OSCCONbits.IRCF1 = 1;
     OSCCONbits.IRCF2 = 1;
-    TRISAbits.TRISA5= 0;
+    ////////////////////////////////////////////////////////////
+    //TRISAbits.TRISA5= 1;
     PORTAbits.RA5 =0;
     PORTA =0;
     ///////////////////////////////////////////////////////////
-    TRISD =0b00000000; //se define el puerto D como salidas
+  //  TRISD =0b00000000; //se define el puerto D como salidas
     //////////////////////////////////////////////////////////////
     TRISCbits.TRISC6 =0; //Tx como salida
     TRISCbits.TRISC7 =1; //Rx como entrada
+    TRISDbits.TRISD0 =0;
     PORTD =0;          //se limpia el puerto D
     ANSELH =0;
     ANSEL=0;
