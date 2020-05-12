9413d8f5b2b19647326b80c5509244e2f97cc637
DeborL17065 <60325411+DeborL17065@users.noreply.github.com>
2020-05-11 23:10:42 -0600

    Update main.c

--- SLAVE_TANQ.X/main.c
+++ SLAVE_TANQ.X/main.c
@@ -34,12 +34,15 @@
 #include <string.h>
 #include <stdlib.h>
 #include "I2C.h"
+#include "ADC.h"
 
 void init(void);
 
 uint8_t z;
 uint8_t dato;
-int VARIABLE = 0;
+uint8_t S_HUMEDAD,S_NIVEL,S_LLUVIA;
+
+char VARIABLE = 0;
 
 //*****************************************************************************
 // Código de Interrupción 
@@ -63,43 +66,39 @@ void __interrupt() isr(void){
             PIR1bits.SSPIF = 0;         // Limpia bandera de interrupción recepción/transmisión SSP
             SSPCONbits.CKP = 1;         // Habilita entrada de pulsos de reloj SCL
             while(!SSPSTATbits.BF);     // Esperar a que la recepción se complete
-////            PORTD = SSPBUF;             // Guardar en el PORTD el valor del buffer de recepción
-////            __delay_us(250);
+         //   PORTD = SSPBUF;             // Guardar en el PORTD el valor del buffer de recepción
+          //  __delay_us(250);
             
         }else if(!SSPSTATbits.D_nA && SSPSTATbits.R_nW){
+            z = SSPBUF;
+            BF = 0;
             if (VARIABLE==0){
-                z = SSPBUF;
-                BF = 0;
-              //  SSPBUF = SENSOR1;
-                SSPCONbits.CKP = 1;
-                __delay_us(250);
-                while(SSPSTATbits.BF);
-                VARIABLE=1;
-                PIR1bits.SSPIF = 0;
-                return; 
+            SSPBUF = V_NIVEL;
+            VARIABLE=1;
+            SSPCONbits.CKP = 1;
+            __delay_us(250);
+            while(SSPSTATbits.BF);
+            PIR1bits.SSPIF = 0;
+            return; 
             }
             if (VARIABLE==1){
-                z = SSPBUF;
-                BF = 0;
-              //  SSPBUF = SENSOR1;
-                SSPCONbits.CKP = 1;
-                __delay_us(250);
-                while(SSPSTATbits.BF);
-                VARIABLE=2;
-                PIR1bits.SSPIF = 0;
-                return; 
+            SSPBUF = V_HUMEDAD;
+            VARIABLE=0;
+            SSPCONbits.CKP = 1;
+            __delay_us(250);
+            while(SSPSTATbits.BF);
+            PIR1bits.SSPIF = 0;
+            return; 
             }
-            if (VARIABLE==2){
-                z = SSPBUF;
-                BF = 0;
-              //  SSPBUF = SENSOR1;
-                SSPCONbits.CKP = 1;
-                __delay_us(250);
-                while(SSPSTATbits.BF);
-                VARIABLE=0;
-                PIR1bits.SSPIF = 0;
-                return; 
-            }
+//            if (VARIABLE==2){
+//                SSPBUF = V_LLUVIA;
+//                VARIABLE=0;
+//                SSPCONbits.CKP = 1;
+//                __delay_us(250);
+//                while(SSPSTATbits.BF);
+//                PIR1bits.SSPIF = 0;
+//                return; 
+//            }
         }
        
         PIR1bits.SSPIF = 0;    
@@ -109,6 +108,7 @@ void __interrupt() isr(void){
 void main(void) {
     init();
     while(1){
+        CONF_ADC();
       
     
     }
@@ -122,6 +122,8 @@ void init(void) {
     OSCCONbits.IRCF2 = 1; 
     ///////////////////////////////////////////////////////////
     TRISAbits.TRISA0 =1; //POTENCIOMETRO 0 
+    TRISAbits.TRISA1 =1; //POTENCIOMETRO 0 
+    TRISAbits.TRISA2 =1; //POTENCIOMETRO 0 
     //////////////////////////////////////////////////////////////
     PORTC =0;            //se limpia el puerto C
     PORTA =0;          //se limpia el puerto A
@@ -129,6 +131,8 @@ void init(void) {
     ANSEL = 0;
     ANSELH =0;
     ANSELbits.ANS0 =1; //RA0 ANALóGICO
+    ANSELbits.ANS1 =1; //RA1 ANALóGICO
+    ANSELbits.ANS2 =1; //RA2 ANALóGICO
     
-    I2C_Slave_Init(0x40); 
+    I2C_Slave_Init(0x80); 
 }
