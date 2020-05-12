c9963ef27b920ddcdb74369f53a88a732077428f
DeborL17065 <60325411+DeborL17065@users.noreply.github.com>
2020-05-11 23:10:47 -0600

    Update main.c

--- MASTER.X/main.c
+++ MASTER.X/main.c
@@ -38,51 +38,69 @@
 #include "USART.h"
 
 #define SWITCH  RB1
+#define SWITCH_AGUA RB5
+#define SWITCH_COMPUERTA RA0
 #define ALARMA  RB2
 #define ALUMBRADO  RB3
+#define PULSADOR   RE2
 
 
 void init(void);
 void read(float LUZ);
+void read3(float LUZ);
+void read2(float LUZ);
+void read4(float LUZ);
 void read1(int m);
-
-int a;
-float S_Luz;
+float S_HUMEDAD,S_NIVEL,S_LLUVIA;
+uint8_t S_ALARMA;
+uint8_t BOTON=0;
+uint8_t Estado1 =0;
+int a=0;
+float  S_Luz;
 
 
 void main(void) {
     init();
+    _RX_TX();
     LCD_INIT();
-    T1CON = 0x10;               //Initialize Timer Module
-    while(1){    
-        Temp = Leer_Sensor(_AMB_TEMP);         // Lee Temp. ambiente
+    T1CON = 0x10;                              //Initialize Timer Module
+    while(1){
+       
+        Leer_Sensor();
+        Temp = Temp_var;                        // Lee Temp. ambiente
         Temp = (Temp * 0.02) - 273.15;         // Convierte a grados Celsius
         __delay_ms(200);
         
-        I2C_Master_RepeatedStart();
+        I2C_Master_Start();
         I2C_Master_Write(0x61);
         S_Luz = I2C_Master_Read(0);
         I2C_Master_Stop();
         __delay_ms(200);
+         
+        I2C_Master_RepeatedStart();
+        I2C_Master_Write(0x81);
+        S_NIVEL = I2C_Master_Read(0);
+        I2C_Master_Stop();
+        __delay_ms(200);
         
-//        
-//         //I2C_Master_Start();
-//        I2C_Master_RepeatedStart();
-//        I2C_Master_Write(0x41);
-//        S_NIVEL = I2C_Master_Read(0);
-////        I2C_Master_Write(0x0F);
-//        I2C_Master_Stop();
-//        __delay_ms(200);
-//       
-//        I2C_Master_Start();
-//        I2C_Master_Write(0x51);
-//        S_CONT = I2C_Master_Read(0);
-//        I2C_Master_Stop();
-//        __delay_ms(200);
-//        PORTB++; 
+        I2C_Master_RepeatedStart();
+        I2C_Master_Write(0x71);
+        S_HUMEDAD = I2C_Master_Read(0);
+        I2C_Master_Stop();
+        __delay_ms(200);
+        
+        I2C_Master_RepeatedStart();
+        I2C_Master_Write(0x81);
+        S_LLUVIA = I2C_Master_Read(0);
+        I2C_Master_Stop();
+        __delay_ms(200);
+        
 
+        
+    //////////////////SENSOR DE HUMEDAD ///////////////////////  
+        S_HUMEDAD = (255 - S_HUMEDAD);
+        S_LLUVIA = (255 - S_LLUVIA);
     //////////////////SENSOR ULTRASONICO/////////////////////////
-
         TMR1H = 0;                //Sets the Initial Value of Timer
         TMR1L = 0;                //Sets the Initial Value of Timer
 
@@ -90,52 +108,108 @@ void main(void) {
         __delay_us(10);           //10uS Delay 
         RB0 = 0;                  //TRIGGER LOW
 
-         while(!RB4);              //Waiting for Echo
+        while(!RB4);              //Waiting for Echo
         TMR1ON = 1;               //Timer Starts
         while(RB4);               //Waiting for Echo goes LOW
         TMR1ON = 0;               //Timer Stops
 
         a = (TMR1L | (TMR1H<<8)); //Reads Timer Value
         a = a/58;              //Converts Time to Distance
-    ///////////////////////////////////////////////////////////////   
-    
-
-    ////////////////  DESPLIEGUE EN LCD  //////////////////////////
-
-        Lcd_Clear();
-        LCD_XY(0,2);
-        LCD_Cadena("S1:  S2:  S3:"); 
-        Mostrar_Temperatura(_AMB_TEMP, Temp);  // Muestra los datos 
+///////////////////////////////////////////////////////////////  
+        S_HUMEDAD = (S_HUMEDAD/255)*100;
         S_Luz = (S_Luz/255)*100;
-        read(S_Luz);
-        read1(a);
-    ////////////////////////////////////////////////////////////////
+        S_NIVEL = (S_NIVEL/255)*100;
+        S_LLUVIA = (S_LLUVIA/255)*100;
         
+        if ((S_NIVEL<90) && (S_LLUVIA!=0)){
+            SWITCH_COMPUERTA =1;
+        }
+        else{
+            SWITCH_COMPUERTA =0;
+        } 
+        if ((S_HUMEDAD<50) && (S_NIVEL>20)){
+            SWITCH_AGUA =1;
+        }
+        else{
+            SWITCH_AGUA =0;
+        }  
         if (Temp>20){
             SWITCH =1;
         }
         else{
             SWITCH =0;
-        }
-        
+        }   
         if (a<12){
+          //  ALUMBRADO=1;
             ALARMA =1;
+            __delay_ms(10);
+         //   ALUMBRADO=0;
+            ALARMA =0;
+            __delay_ms(10);
+            S_ALARMA =1;
+         
         }
         else{
             ALARMA =0;
+            S_ALARMA =0;
+         
         }
-        if (S_Luz<75){
+        if (S_Luz>75){
             ALUMBRADO =1;
         }
         else{
             ALUMBRADO =0;
         }
-
+ ////////////////  DESPLIEGUE EN LCD  //////////////////////////
+        if (PULSADOR ==1){   
+            Estado1 =1;
+        }
+        if (Estado1 ==1 && PULSADOR ==0){ //boton con antirebote
+            Estado1 =0;
+            BOTON++ ;      //se incrementa el contador
+        }  
+        if (BOTON ==0){
+            Lcd_Clear();
+            LCD_XY(0,0);
+            LCD_Cadena("TEMP:  LUZ:  ALA:"); 
+            Mostrar_Temperatura(_AMB_TEMP, Temp);  // Muestra los datos 
+            read3(S_Luz);
+            read1(a);
         
-  //      LCD_XY(1,12);
-   //     LCD_Cadena(SN);//manda el dato a la LCD
-//
-//    
+        }
+        if (BOTON ==1){
+      
+            Lcd_Clear();
+            LCD_XY(0,0);
+            LCD_Cadena("HUM:  NIV:  LLU:"); 
+            read2(S_HUMEDAD);
+            read(S_NIVEL);
+            read4(S_LLUVIA);
+        
+        }
+        if (BOTON ==2){ 
+            BOTON =0;
+        }
+/////////////////////////////////////////////////////////////// 
+        Temp = (Temp/100)*255;
+        S_Luz = (S_Luz/100)*255;
+        S_HUMEDAD = (S_HUMEDAD/100)*255;
+        S_LLUVIA = (S_LLUVIA/100)*255;
+        S_NIVEL = (S_NIVEL/100)*255;
+
+        SerialSendChar(Temp); // Temp
+        __delay_ms(1);
+        SerialSendChar(S_Luz);
+        __delay_ms(1);
+        SerialSendChar(S_HUMEDAD); ///Humedad
+        __delay_ms(1);
+        SerialSendChar(S_LLUVIA); ///Lluvia
+        __delay_ms(1);
+        SerialSendChar(S_NIVEL); //nivel
+        __delay_ms(1);
+        SerialSendChar(Temp); //alarma
+        __delay_ms(1);
+  ////////////////////////////////////////////////////////////////          
     }
     return;
 }
@@ -146,11 +220,23 @@ void init(void) {
     OSCCONbits.IRCF1 = 1;
     OSCCONbits.IRCF2 = 1; 
     ///////////////////////////////////////////////////////////
+    
+    TRISEbits.TRISE2 =1; // entrada  
     TRISD =0b00000000; //se define el puerto D como salidas
-    TRISB =0b00011110;     
+    //TRISB =0b0001000; 
+    TRISBbits.TRISB0 =0;
+    TRISBbits.TRISB4 =1;
+    TRISBbits.TRISB2 =0;
+    TRISBbits.TRISB3 =0;
+    TRISBbits.TRISB5 =0;
+    TRISBbits.TRISB1 =0;
+    TRISAbits.TRISA0 =0; //Tx como salida
+    TRISCbits.TRISC6 =0; //Tx como salida
+    TRISCbits.TRISC7 =1; //Rx como entrada  
     //////////////////////////////////////////////////////////////
     PORTC =0;           //se limpia el puerto C
     PORTD =0;          //se limpia el puerto D
+    PORTEbits.RE2 =0;
     //////////////////////////////////////////////////////////
     ANSEL = 0;
     ANSELH =0;
@@ -176,14 +262,65 @@ void read(float LUZ){
     strcat(PUNTO1,SA); //se concatenan ambos chars
     strcat(SB,PUNTO1); //se concatenan ambos chars
     strcat(SC,SB);
+    LCD_XY(1,6);
+    LCD_Cadena(SC);//manda el dato a la LCD
+}
+void read3(float LUZ){
+    char SA[5],SB[5],SC[5],PUNTO1[5];
+    int A_,B_,C_,NUM1_;
+    NUM1_ =(LUZ)*10;
+    A_ = NUM1_%10;   //8
+    itoa(SA,A_,10);
+    B_ = (NUM1_/10)%10;//9
+    itoa(SB,B_,10);
+    C_ = (NUM1_/100)%10;//1 
+    itoa(SC,C_,10);
+    strcpy(PUNTO1,"."); //se coloca el caracter dentro del char
+    strcat(PUNTO1,SA); //se concatenan ambos chars
+    strcat(SB,PUNTO1); //se concatenan ambos chars
+    strcat(SC,SB);
     LCD_XY(1,7);
     LCD_Cadena(SC);//manda el dato a la LCD
 }
 
+void read2(float LUZ){
+    char SA[5],SB[5],SC[5],PUNTO1[5];
+    int A_,B_,C_,NUM1_;
+    NUM1_ =(LUZ)*10;
+    A_ = NUM1_%10;   //8
+    itoa(SA,A_,10);
+    B_ = (NUM1_/10)%10;//9
+    itoa(SB,B_,10);
+    C_ = (NUM1_/100)%10;//1 
+    itoa(SC,C_,10);
+    strcpy(PUNTO1,"."); //se coloca el caracter dentro del char
+    strcat(PUNTO1,SA); //se concatenan ambos chars
+    strcat(SB,PUNTO1); //se concatenan ambos chars
+    strcat(SC,SB);
+    LCD_XY(1,0);
+    LCD_Cadena(SC);//manda el dato a la LCD
+}
+
+void read4(float LUZ){
+    char SA[5],SB[5],SC[5],PUNTO1[5];
+    int A_,B_,C_,NUM1_;
+    NUM1_ =(LUZ)*10;
+    A_ = NUM1_%10;   //8
+    itoa(SA,A_,10);
+    B_ = (NUM1_/10)%10;//9
+    itoa(SB,B_,10);
+    C_ = (NUM1_/100)%10;//1 
+    itoa(SC,C_,10);
+    strcpy(PUNTO1,"."); //se coloca el caracter dentro del char
+    strcat(PUNTO1,SA); //se concatenan ambos chars
+    strcat(SB,PUNTO1); //se concatenan ambos chars
+    strcat(SC,SB);
+    LCD_XY(1,12);
+    LCD_Cadena(SC);//manda el dato a la LCD
+}
 void read1(int m){
     char SC[5];
     sprintf(SC,"%3u",m);
     LCD_XY(1,12);   
     LCD_Cadena(SC);//manda el dato a la LCD
-
 }
